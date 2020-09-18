691496da4b11cc59f7b39d638fd9dc401ed6e232
notskamr <66314478+notskamr@users.noreply.github.com>
2020-09-19 01:15:03 +0530

    2

--- js/Particle.js
+++ js/Particle.js
@@ -1,13 +1,13 @@
 class Particle {
-    constructor(x, y,r) {
+    constructor(x, y, r) {
 
-        var options ={
-            restitution:0.4
+        var options = {
+            restitution: 0.4
         }
-        this.r=r;
-      
-        this.body = Bodies.circle(x, y, this.r,options);       
-        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        this.r = r;
+
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
 
     }
@@ -23,7 +23,7 @@ class Particle {
         noStroke();
         fill(this.color)
         ellipseMode(RADIUS);
-        ellipse(0, 0, this.r,this.r);
+        ellipse(0, 0, this.r, this.r);
         pop();
     }
 
--- js/Plinko.js
+++ js/Plinko.js
@@ -3,11 +3,11 @@ class Plinko {
         var options = {
             restitution: 1,
             friction: 0,
-            isStatic:true
+            isStatic: true
         }
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
-        
+
         World.add(world, this.body);
     }
     display() {
@@ -22,7 +22,7 @@ class Plinko {
         noStroke();
         fill("white");
         ellipseMode(RADIUS);
-        ellipse(0,0,this.r,this.r);
+        ellipse(0, 0, this.r, this.r);
         pop();
     }
 
